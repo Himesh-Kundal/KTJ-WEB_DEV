e71ac461fdf2bed129bada0ab07bd9af37066724
Himesh-Kundal <himesh.kundal@kgpian.iitkgp.ac.in>
2024-06-14 13:48:31 +0530

    game mech done

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,3 +1,5 @@
+let gameEnded=false;
+
 const table=document.querySelector(".main-table");
 
 const player1={
@@ -35,23 +37,60 @@ table.innerHTML=`
 
 const cells=document.querySelectorAll(".cell");
 
-check(move){
+const jsCell=[];
 
-};
+for(let i=0;i<9;i++){
+    jsCell.push(document.querySelector(`.js-cell-${i+1}`));
+}
 
+function check(move){
+    for(let i=0;i<3;i++){
+        if(jsCell[i].dataset.move===move && jsCell[i+3].dataset.move===move && jsCell[i+6].dataset.move===move){
+            console.log(`${move} wins`);
+            return "stop";
+        }
+    }
+    for(let i=0;i<jsCell.length;i+=3){
+        if(jsCell[i].dataset.move===move && jsCell[i+1].dataset.move===move && jsCell[i+2].dataset.move===move){
+            console.log(`${move} wins`);
+            return "stop";
+        }
+    }
+    if(jsCell[0].dataset.move===move && jsCell[4].dataset.move===move && jsCell[8].dataset.move===move){
+        console.log(`${move} wins`);
+        return "stop";
+    }
+    if(jsCell[2].dataset.move===move && jsCell[4].dataset.move===move && jsCell[6].dataset.move===move){
+        console.log(`${move} wins`);
+        return "stop";
+    }
+    if(jsCell.every(cell=>cell.dataset.move!=="")){
+        console.log("Draw");
+        return "stop";
+    }
+    else{
+        return "continue";
+    }
+}
 
+const play= (cell)=>{
+    if(gameEnded){
+        return;
+    }
 
-
-
+    if(cell.innerHTML!==""){
+        return;
+    }
+    cell.innerHTML=`${currentPlayer.icon}`;
+    cell.dataset.move=currentPlayer.move;
+    const result = check(currentPlayer.move);
+    if(result==="stop"){
+        gameEnded=true ;
+        return;
+    }
+    currentPlayer=currentPlayer===player1?player2:player1; 
+}
 
 cells.forEach((cell) => {
-    cell.addEventListener("click",()=>{
-        if(cell.innerHTML!==""){
-            return;
-        }
-        cell.innerHTML=`${currentPlayer.icon}`;
-        cell.dataset.move=currentPlayer.move;
-        check(currentPlayer.move);
-        currentPlayer=currentPlayer===player1?player2:player1; 
-    });
+    cell.addEventListener("click",play.bind(null,cell));
 });
